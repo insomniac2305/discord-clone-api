e76073434106e4a9a3040db5372ee373205476fc
insomniac2305 <84805847+insomniac2305@users.noreply.github.com>
2023-10-10 23:51:48 +0200

    Add CORS support

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 const mongoose = require("mongoose");
 
 const indexRouter = require("./routes");
@@ -21,6 +22,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 app.use("/api", apiRouter);
 app.use("/", indexRouter);
