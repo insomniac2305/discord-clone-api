dcb14ed25b617d7bf55c0b3b3f339a3127d8c0d7
insomniac2305 <84805847+insomniac2305@users.noreply.github.com>
2023-11-05 00:57:05 +0100

    Remove authentication for server files

--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,10 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 const fileController = require("../controllers/fileController");
-const serverController = require("../controllers/serverController");
-const { checkAuth } = require("../middleware/auth");
 
 router.get("/login", authController.getLogin);
 
 router.post("/login", authController.postLogin);
-
-router.get(
-  "/files/servers/:serverid/:fileName",
-  checkAuth(),
-  serverController.addParamServerToReq,
-  serverController.checkServerPermissions()
-);
 
 router.get("/files/:directory/:id/:fileName", fileController.getFile);
 
