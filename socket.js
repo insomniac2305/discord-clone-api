9d18c38296dccf52ad7a63b70bee0a2c4a2848fd
insomniac2305 <84805847+insomniac2305@users.noreply.github.com>
2023-11-30 22:36:47 +0100

    Add token auth for joinChannel event

--- socket.js
+++ socket.js
@@ -1,19 +1,47 @@
 require("dotenv").config();
-const { Server } = require("socket.io");
-const { checkAuth } = require("./middleware/auth");
+const socketio = require("socket.io");
+const jwt = require("jsonwebtoken");
+const Server = require("./models/Server");
 
 let io;
 
 exports.initSocketIO = (httpServer) => {
-  io = new Server(httpServer, {
+  io = new socketio.Server(httpServer, {
     cors: {
       origin: process.env.CLIENT_URL,
     },
   });
 
   io.on("connection", (socket) => {
-    socket.on("joinChannel", (channelId) => {
-      socket.join(channelId);
+    socket.on("joinChannel", (channelId, token) => {
+      const secret = process.env.JWT_SECRET;
+      jwt.verify(token, secret, async (jwtError, payload) => {
+        if (jwtError) {
+          console.error("Could not verify jwt: " + jwtError);
+          return;
+        }
+
+        const userId = payload.sub;
+        let server;
+
+        try {
+          server = await Server.findOne({ channels: channelId });
+          if (!server) {
+            console.log("Server not found with channel: " + channelId);
+            return;
+          }
+        } catch (serverError) {
+          console.error("Could not find user from jwt: " + serverError);
+        }
+
+        const isMember = server.members.find((member) => member.user.toString() === userId);
+
+        if (isMember) {
+          socket.join(channelId);
+        } else {
+          console.log(`User ${userId} is not member of server ${server._id}, socket not joining channel.`);
+        }
+      });
     });
 
     socket.on("leaveChannel", (channelId) => {
